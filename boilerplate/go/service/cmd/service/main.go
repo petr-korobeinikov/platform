06817fc46e5f0c7c90422a8f4c1fe1f927ff7340
Petr Korobeinikov <pkorobeinikov@gmail.com>
2021-11-13 22:38:26 +0300

    Boilerplate: go (metrics).

--- boilerplate/go/service/cmd/service/main.go
+++ boilerplate/go/service/cmd/service/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
@@ -10,6 +14,8 @@ func main() {
 	http.HandleFunc("/healthz", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte(`{"status": "ok"}`))
 	})
+
+	http.Handle("/metrics", promhttp.Handler())
 
 	http.ListenAndServe(":9000", nil)
 }
