f283ece24c3c0f0a23fa567ea5a1189a529b1057
Petr Korobeinikov <pkorobeinikov@gmail.com>
2021-12-25 11:21:17 +0300

    Showcase: reference-observable-service (configure jaeger).

--- platform-showcase/reference-observable-service/cmd/service/main.go
+++ platform-showcase/reference-observable-service/cmd/service/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo-contrib/jaegertracing"
+	"github.com/labstack/echo/v4"
+)
+
+func init() {
+	_ = godotenv.Load(".platform/env/.env", ".env")
+}
+
 func main() {
-	println("Hello, I am reference-observable-service!")
+	e := echo.New()
+
+	c := jaegertracing.New(e, nil)
+	defer c.Close()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello!")
+	})
+
+	e.Logger.Fatal(e.Start(os.Getenv("LISTEN_ON")))
 }
