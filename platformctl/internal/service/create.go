cda6244bae2aff989e005cc5c05126e68131f049
Petr Korobeinikov <pkorobeinikov@gmail.com>
2021-12-25 10:39:32 +0300

    Subcommand: service create (create service spec).

--- platformctl/internal/service/create.go
+++ platformctl/internal/service/create.go
@@ -7,45 +7,64 @@ import (
 	"path"
 
 	"github.com/pkorobeinikov/platform/platform-lib/filesystem"
+	"github.com/pkorobeinikov/platform/platform-lib/service/spec"
 	"github.com/pkorobeinikov/platform/platform-lib/service/validation"
 )
 
-func Create(ctx context.Context, serviceName string) error {
-	if err := validation.EnsureServiceNameValid(serviceName); err != nil {
-		return err
+func Create(ctx context.Context, serviceName string) (err error) {
+	if err = validation.EnsureServiceNameValid(serviceName); err != nil {
+		return
 	}
 
 	if filesystem.IsDirectoryExists(serviceName) {
 		return errServiceDirectoryAlreadyExists
 	}
 
-	if err := filesystem.MkDir(serviceName); err != nil {
-		return err
+	if err = filesystem.MkDir(serviceName); err != nil {
+		return
 	}
 
 	// region Service Template
-	if err := filesystem.MkDir(path.Join(serviceName, "cmd")); err != nil {
-		return err
+	if err = filesystem.MkDir(path.Join(serviceName, "cmd")); err != nil {
+		return
 	}
 
-	err := filesystem.Touch(
+	err = filesystem.Touch(
 		path.Join(serviceName, "go.mod"),
 		filesystem.WithContentsOfString(
 			fmt.Sprintf(gomodFmt, serviceName),
 		),
 	)
 	if err != nil {
-		return err
+		return
+	}
+
+	err = filesystem.Touch(
+		path.Join(serviceName, spec.File),
+		filesystem.WithContentsOfString(
+			fmt.Sprintf(specFmt, serviceName),
+		),
+	)
+	if err != nil {
+		return
 	}
 	// endregion
 
-	return nil
+	return
 }
 
 const (
 	gomodFmt = `module %s
 
 go 1.17
+`
+
+	specFmt = `name: %s
+
+environment:
+  _: ~
+  local: ~
+  staging: ~
 `
 )
 
