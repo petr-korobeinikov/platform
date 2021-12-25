ac702f199301660f3e08f9c4910486ae49658552
Petr Korobeinikov <pkorobeinikov@gmail.com>
2021-12-25 17:05:22 +0300

    Showcase: reference-observable-service (fib tests).

--- platform-showcase/reference-observable-service/internal/service/fibonacci/fib_test.go
+++ platform-showcase/reference-observable-service/internal/service/fibonacci/fib_test.go
@@ -1,6 +1,39 @@
 package fibonacci_test
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	. "reference-observable-service/internal/service/fibonacci"
+)
 
 func TestCountingService_Count(t *testing.T) {
+	t.Run("positive", func(t *testing.T) {
+		ctx := context.Background()
+
+		sut := NewCountingService(10)
+		_, err := sut.Count(ctx, 3)
+
+		assert.NoError(t, err)
+	})
+
+	t.Run("too distant number", func(t *testing.T) {
+		ctx := context.Background()
+
+		sut := NewCountingService(10)
+		_, err := sut.Count(ctx, 999)
+
+		assert.ErrorIs(t, err, ErrFibonacciNumberIsTooDistant)
+	})
+
+	t.Run("negative", func(t *testing.T) {
+		ctx := context.Background()
+
+		sut := NewCountingService(10)
+		_, err := sut.Count(ctx, -1)
+
+		assert.ErrorIs(t, err, ErrFibonacciNumberIsNegative)
+	})
 }
