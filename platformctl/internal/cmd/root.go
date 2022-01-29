b5594d9ff325c2cf3e11937bbc59d7d536f3b033
Petr Korobeinikov <pkorobeinikov@gmail.com>
2022-01-29 11:21:58 +0300

    Platformctl: fix config help message.

--- platformctl/internal/cmd/root.go
+++ platformctl/internal/cmd/root.go
@@ -46,7 +46,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.platformctl/.platformctl.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.platformctl/platformctl.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
