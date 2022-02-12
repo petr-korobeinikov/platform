8a73462d1d997675284d7bafdfb748bc3e1b2496
Petr Korobeinikov <pkorobeinikov@gmail.com>
2022-02-12 07:51:16 +0300

    Subcommand: task (lint showcase).

--- platform-showcase/task-providing-service/cmd/service/main.go
+++ platform-showcase/task-providing-service/cmd/service/main.go
@@ -2,4 +2,19 @@ package main
 
 func main() {
 	println("Hello, I am task-providing-service!")
+
+	// ineffectual assignment
+	a := 1
+	a = 2
+	println(a)
+
+	// dogsled
+	_, _, _, _, err := dogsled()
+	if err != nil {
+		return
+	}
+}
+
+func dogsled() (int, int, int, int, error) {
+	return 0, 0, 0, 0, nil
 }
