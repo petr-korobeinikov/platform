6725fe393ee1903b68dda4992f2ff3ebb56c4a10
Petr Korobeinikov <pkorobeinikov@gmail.com>
2022-02-22 21:05:18 +0300

    Subcommand: service (common pre run).

--- platformctl/internal/cmd/service.go
+++ platformctl/internal/cmd/service.go
@@ -1,10 +1,15 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+
+	"platformctl/internal/service"
+)
 
 var serviceCmd = &cobra.Command{
-	Use:   "service",
-	Short: "Operations with services",
+	Use:               "service",
+	Short:             "Operations with services",
+	PersistentPreRunE: service.PersistentPreRunE,
 }
 
 func init() {
---
+++ platformctl/internal/service/prerun.go
@@ -0,0 +1,11 @@
+package service
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func PersistentPreRunE(cmd *cobra.Command, args []string) error {
+	// Ensure platform started
+
+	return nil
+}
