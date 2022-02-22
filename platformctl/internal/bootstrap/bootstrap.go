3983cb78f7112e6dddc52db0c50eddb79f4d69d5
Petr Korobeinikov <pkorobeinikov@gmail.com>
2022-02-23 00:38:56 +0300

    Subcommand: bootstrap (fetch config).

--- platformctl/internal/bootstrap/bootstrap.go
+++ platformctl/internal/bootstrap/bootstrap.go
@@ -2,9 +2,20 @@ package bootstrap
 
 import (
 	"context"
-	"errors"
+	"path"
+
+	"github.com/carlmjohnson/requests"
+	"github.com/mitchellh/go-homedir"
 )
 
 func Bootstrap(ctx context.Context, bootstrapURL string) error {
-	return errors.New("command not implemented")
+	home, err := homedir.Dir()
+	if err != nil {
+		return err
+	}
+
+	return requests.
+		URL(bootstrapURL).
+		ToFile(path.Join(home, ".platformctl", "platformctl.yaml")).
+		Fetch(ctx)
 }
